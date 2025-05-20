8850a39fa42fa069d84f98fcd0d8003446780551
myhelper65 <myhelper65@gmail.com>
2025-05-20 14:28:26 +0000

    added script for creating ECR registry for dev

--- build-dev-docker-images.sh
+++ build-dev-docker-images.sh
@@ -3,3 +3,5 @@
 docker build -t "events-service:dev" ./events-service
 docker build -t "registration-service:dev" ./registration-service
 
+#If you want both: build images individually first then use Docker Compose, you can do Then bring up your full environment
+# docker-compose up
--- jenkins/jenkins-petclinic-ci-job.sh
+++ jenkins/jenkins-petclinic-ci-job.sh
@@ -1,8 +1,10 @@
 echo 'Running Unit Tests on event server Application'
+
 docker run --rm \
-  -v /var/lib/jenkins/.m2:/root/.m2 \
-  -v /var/lib/jenkins/workspace/event-ci-job:/app \
+  -v $HOME/.m2:/root/.m2 \
+  -v "$(pwd)":/app \
   -w /app \
   maven:3.9.6-eclipse-temurin-21 \
-  mvn clean test -Dspring.profiles.active=docker
+  mvn clean test
+
 
