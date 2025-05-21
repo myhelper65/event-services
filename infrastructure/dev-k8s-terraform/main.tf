bc11f95f02d83f93ee36f452cec5556f5a4da915
myhelper65 <myhelper65@gmail.com>
2025-05-21 22:15:21 +0000

    fix main.tf

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 variable "sec-gr-k8s" {
@@ -11,7 +11,7 @@ data "aws_vpc" "name" {
 }
 
 resource "aws_security_group" "k8s-sec-gr" {
-  name = var.sec-gr-k8s
+  name   = var.sec-gr-k8s
   vpc_id = data.aws_vpc.name.id
   tags = {
     Name = var.sec-gr-k8s
@@ -21,7 +21,7 @@ resource "aws_security_group" "k8s-sec-gr" {
     from_port = 0
     protocol  = "-1"
     to_port   = 0
-    self = true
+    self      = true
   }
 
   ingress {
@@ -32,9 +32,9 @@ resource "aws_security_group" "k8s-sec-gr" {
   }
 
   ingress {
-    protocol = "tcp"
-    from_port = 6443
-    to_port = 6443
+    protocol    = "tcp"
+    from_port   = 6443
+    to_port     = 6443
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -54,7 +54,7 @@ resource "aws_security_group" "k8s-sec-gr" {
 }
 
 
-resource "aws_iam_role" "eventserver-master-server-s3-role" {
+resource "aws_iam_role" "eventserver_ansible_test_dev_keyserver_master_server_s3_role" {
   name               = "eventserver-master-server-role"
   assume_role_policy = <<EOF
 {
@@ -71,8 +71,8 @@ resource "aws_iam_role" "eventserver-master-server-s3-role" {
   ]
 }
 EOF
-
 }
+
 
 resource "aws_iam_role_policy_attachment" "eventserver_s3_policy" {
   role       = aws_iam_role.eventserver-master-server-s3-role.name
@@ -85,67 +85,67 @@ resource "aws_iam_instance_profile" "eventserver-master-server-profile" {
 }
 
 resource "aws_instance" "kube-master" {
-  ami = "ami-005fc0f236362e99f"
-  instance_type = "t3a.medium"
-  iam_instance_profile = aws_iam_instance_profile.eventserver-master-server-profile.name
+  ami                    = "ami-005fc0f236362e99f"
+  instance_type          = "t3a.medium"
+  iam_instance_profile   = aws_iam_instance_profile.eventserver-master-server-profile.name
   vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
-  key_name = "event"
-  subnet_id = "subnet-042907c137a98e049" # select own subnet_id of us-east-1a
-  availability_zone = "us-east-1a"
+  key_name               = "event"
+  subnet_id              = "subnet-042907c137a98e049" # select own subnet_id of us-east-1a
+  availability_zone      = "us-east-1a"
   tags = {
-    Name = "kube-master"
-    Project = "tera-kube-ans"
-    Role = "master"
-    Id = "1"
+    Name        = "kube-master"
+    Project     = "tera-kube-ans"
+    Role        = "master"
+    Id          = "1"
     environment = "dev"
   }
 }
 
 resource "aws_instance" "worker-1" {
-  ami = "ami-005fc0f236362e99f"
-  instance_type = "t3a.medium"
+  ami                    = "ami-005fc0f236362e99f"
+  instance_type          = "t3a.medium"
   vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
-  key_name = "event"
-  subnet_id = "subnet-042907c137a98e049"  # select own subnet_id of us-east-1a
-  availability_zone = "us-east-1a"
+  key_name               = "event"
+  subnet_id              = "subnet-042907c137a98e049" # select own subnet_id of us-east-1a
+  availability_zone      = "us-east-1a"
   tags = {
-    Name = "worker-1"
-    Project = "tera-kube-ans"
-    Role = "worker"
-    Id = "1"
+    Name        = "worker-1"
+    Project     = "tera-kube-ans"
+    Role        = "worker"
+    Id          = "1"
     environment = "dev"
   }
 }
 
 resource "aws_instance" "worker-2" {
-  ami = "ami-005fc0f236362e99f"
-  instance_type = "t3a.medium"
+  ami                    = "ami-005fc0f236362e99f"
+  instance_type          = "t3a.medium"
   vpc_security_group_ids = [aws_security_group.k8s-sec-gr.id]
-  key_name = "event"
-  subnet_id = "subnet-042907c137a98e049"  # select own subnet_id of us-east-1a
-  availability_zone = "us-east-1a"
+  key_name               = "event"
+  subnet_id              = "subnet-042907c137a98e049" # select own subnet_id of us-east-1a
+  availability_zone      = "us-east-1a"
   tags = {
-    Name = "worker-2"
-    Project = "tera-kube-ans"
-    Role = "worker"
-    Id = "2"
+    Name        = "worker-2"
+    Project     = "tera-kube-ans"
+    Role        = "worker"
+    Id          = "2"
     environment = "dev"
   }
 }
 
-output kube-master-ip {
+output "kube-master-ip" {
   value       = aws_instance.kube-master.public_ip
   sensitive   = false
   description = "public ip of the kube-master"
 }
 
-output worker-1-ip {
+output "worker-1-ip" {
   value       = aws_instance.worker-1.public_ip
   sensitive   = false
   description = "public ip of the worker-1"
 }
 
-output worker-2-ip {
+output "worker-2-ip" {
   value       = aws_instance.worker-2.public_ip
   sensitive   = false
   description = "public ip of the worker-2"
