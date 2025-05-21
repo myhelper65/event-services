cb600897d32ce3aa6934f9220ae8ece3877addc1
myhelper65 <myhelper65@gmail.com>
2025-05-21 22:45:16 +0000

    added maintf

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -74,12 +74,12 @@ EOF
 
 }
 
-resource "aws_iam_role_policy_attachment" "eventserver_s3_policy" {
+resource "aws-iam-role-policy-attachment" "eventserver-s3-policy" {
   role       = aws_iam_role.eventserver_master_server_s3-role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
 
-resource "aws_iam_instance_profile" "eventserver_master_server_profile" {
+resource "aws-iam-instance-profile" "eventserver-master-server-profile" {
   name = "eventserver-master-server-profile"
   role = aws_iam_role.eventserver-master-server-s3-role.name
 }
