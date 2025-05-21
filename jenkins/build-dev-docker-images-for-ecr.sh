133df1741b41c5c17ba6ab9bd9f4ad81890340c0
myhelper65 <myhelper65@gmail.com>
2025-05-21 01:37:46 +0000

    added k8 ,terraform ansible files

---
+++ jenkins/build-dev-docker-images-for-ecr.sh
@@ -0,0 +1,2 @@
+docker build  -t "${IMAGE_TAG_ADMIN_SERVER}" "${WORKSPACE}/spring-petclinic-admin-server"
+docker build  -t "${IMAGE_TAG_API_GATEWAY}" "${WORKSPACE}/spring-petclinic-api-gateway"
---
+++ jenkins/push-dev-docker-images-to-ecr.sh
@@ -0,0 +1,3 @@
+aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REGISTRY} 
+docker push "${IMAGE_TAG_ADMIN_SERVER}"
+docker push "${IMAGE_TAG_API_GATEWAY}"
