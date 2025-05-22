8336f3a6db865048aeac020b3ff27e36b2f14101
myhelper65 <myhelper65@gmail.com>
2025-05-22 17:11:44 +0000

    added qa automation pipeline for dev

--- jenkins/prepare-tags-ecr-for-dev-docker-images.sh
+++ jenkins/prepare-tags-ecr-for-dev-docker-images.sh
@@ -1,7 +1,5 @@
-# EVENTS-SERVICE
 MVN_VERSION=$(. ${WORKSPACE}/events-service/target/maven-archiver/pom.properties && echo $version)
 export IMAGE_TAG_EVENTS="${ECR_REGISTRY}/${APP_REPO_NAME}:events-service-v${MVN_VERSION}-b${BUILD_NUMBER}"
 
-# REGISTRATION-SERVICE
 MVN_VERSION=$(. ${WORKSPACE}/registration-service/target/maven-archiver/pom.properties && echo $version)
 export IMAGE_TAG_REGISTRATION="${ECR_REGISTRY}/${APP_REPO_NAME}:registration-service-v${MVN_VERSION}-b${BUILD_NUMBER}"
