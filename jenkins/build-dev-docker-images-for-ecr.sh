16198a8440cb2a4360b2b5401314504caec370fe
myhelper65 <myhelper65@gmail.com>
2025-05-22 18:33:28 +0000

    added qa automation pipeline for dev

--- jenkins/build-dev-docker-images-for-ecr.sh
+++ jenkins/build-dev-docker-images-for-ecr.sh
@@ -1,6 +1,2 @@
-echo "Building events-service image: ${IMAGE_TAG_EVENTS}"
 docker build -t "${IMAGE_TAG_EVENTS}" "${WORKSPACE}/events-service"
-
-# Build registration-service Docker image
-echo "Building events-service image: ${IMAGE_TAG_REGISTRATION}
 docker build -t "${IMAGE_TAG_REGISTRATION}" "${WORKSPACE}/registration-service"
