eebab92e8de6b28c814834c8e6292dd3128d8f49
myhelper65 <myhelper65@gmail.com>
2025-05-22 16:18:59 +0000

    update the image ecr

--- jenkins/build-dev-docker-images-for-ecr.sh
+++ jenkins/build-dev-docker-images-for-ecr.sh
@@ -1,7 +1,7 @@
 
 
 # Build events-service Docker image
-docker build -t "${IMAGE_TAG_EVENTS}" "${WORKSPACE}/events-service"
+docker build -t "${IMAGE_TAG_EVENTS}""${WORKSPACE}/events-service"
 
 # Build registration-service Docker image
-docker build -t "${IMAGE_TAG_REGISTRATION}" "${WORKSPACE}/registration-service"
+docker build -t "${IMAGE_TAG_REGISTRATION}""${WORKSPACE}/registration-service"
