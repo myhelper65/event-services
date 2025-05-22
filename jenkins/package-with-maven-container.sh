747f1190fe1661ec7ba36b3ad3eb4e5725979523
myhelper65 <myhelper65@gmail.com>
2025-05-22 22:02:39 +0000

    added skipping test in jenkins/package-with-maven-container.sh

--- jenkins/package-with-maven-container.sh
+++ jenkins/package-with-maven-container.sh
@@ -1,2 +1,9 @@
- docker run --rm -v $HOME/.m2:/root/.m2 -v $WORKSPACE:/app -w /app maven:3.9.6-eclipse-temurin-21  mvn clean package
+#  docker run --rm -v $HOME/.m2:/root/.m2 -v $WORKSPACE:/app -w /app maven:3.9.6-eclipse-temurin-21  mvn clean package
 
+
+docker run --rm \
+  -v $HOME/.m2:/root/.m2 \
+  -v $WORKSPACE:/app \
+  -w /app \
+  maven:3.9.6-eclipse-temurin-21 \
+  mvn clean package -Dspring.profiles.active=docker -DskipTests
