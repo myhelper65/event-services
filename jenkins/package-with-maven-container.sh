1e113949634e5434711a60d483d43cddef386850
myhelper65 <myhelper65@gmail.com>
2025-05-22 21:43:34 +0000

    added docker profile jenkins/package-with-maven-container.sh

--- jenkins/package-with-maven-container.sh
+++ jenkins/package-with-maven-container.sh
@@ -1,1 +1,9 @@
-docker run --rm -v $HOME/.m2:/root/.m2 -v $WORKSPACE:/app -w /app maven:3.9.6-eclipse-temurin-21  mvn clean package
+# docker run --rm -v $HOME/.m2:/root/.m2 -v $WORKSPACE:/app -w /app maven:3.9.6-eclipse-temurin-21  mvn clean package
+
+#below command will ensure to user docker profile docker
+docker run --rm \
+  -v $HOME/.m2:/root/.m2 \
+  -v $WORKSPACE:/app \
+  -w /app \
+  maven:3.9.6-eclipse-temurin-21 \
+  mvn clean package -Dspring.profiles.active=docker
