ec0632bcd8c935ff23232c17a020390c39392479
myhelper65 <myhelper65@gmail.com>
2025-05-24 01:58:55 +0000

    added ecr

--- jenkins/prepare-tags-ecr-for-dev-docker-images.sh
+++ jenkins/prepare-tags-ecr-for-dev-docker-images.sh
@@ -4,8 +4,15 @@
 # MVN_VERSION=$(. ${WORKSPACE}/registration-service/target/maven-archiver/pom.properties && echo $version)
 # export IMAGE_TAG_REGISTRATION="${ECR_REGISTRY}/${APP_REPO_NAME}:registration-service-v${MVN_VERSION}-b${BUILD_NUMBER}"
 
-MVN_VERSION=$(grep '^version=' "${WORKSPACE}/events-service/target/maven-archiver/pom.properties" | cut -d'=' -f2)
-export IMAGE_TAG_EVENTS="${ECR_REGISTRY}/${APP_REPO_NAME}:events-service-v${MVN_VERSION}-b${BUILD_NUMBER}"
+# Extract Maven version for events-service
+MVN_VERSION_EVENTS=$(grep '^version=' "${WORKSPACE}/events-service/target/maven-archiver/pom.properties" | cut -d'=' -f2 | xargs)
+export IMAGE_TAG_EVENTS="${ECR_REGISTRY}/${APP_REPO_NAME}:events-service-v${MVN_VERSION_EVENTS}-b${BUILD_NUMBER}"
 
-MVN_VERSION=$(grep '^version=' "${WORKSPACE}/registration-service/target/maven-archiver/pom.properties" | cut -d'=' -f2)
-export IMAGE_TAG_REGISTRATION="${ECR_REGISTRY}/${APP_REPO_NAME}:registration-service-v${MVN_VERSION}-b${BUILD_NUMBER}"
+# Extract Maven version for registration-service
+MVN_VERSION_REGISTRATION=$(grep '^version=' "${WORKSPACE}/registration-service/target/maven-archiver/pom.properties" | cut -d'=' -f2 | xargs)
+export IMAGE_TAG_REGISTRATION="${ECR_REGISTRY}/${APP_REPO_NAME}:registration-service-v${MVN_VERSION_REGISTRATION}-b${BUILD_NUMBER}"
+
+# Debug output
+echo "Tag for events-service:        [$IMAGE_TAG_EVENTS]"
+echo "Tag for registration-service:  [$IMAGE_TAG_REGISTRATION]"
+
