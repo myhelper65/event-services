3c32a6be91e11f81fdb9ea575e89bccf86ce6424
myhelper65 <myhelper65@gmail.com>
2025-05-24 01:44:56 +0000

    added jenkins/build-dev-docker-images-for-ecr.sh

--- jenkins/build-dev-docker-images-for-ecr.sh
+++ jenkins/build-dev-docker-images-for-ecr.sh
@@ -1,2 +1,2 @@
-docker build  -t "${IMAGE_TAG_ADMIN_SERVER}" "${WORKSPACE}/spring-petclinic-admin-server"
-docker build  -t "${IMAGE_TAG_API_GATEWAY}" "${WORKSPACE}/spring-petclinic-api-gateway"
+docker build -t "${IMAGE_TAG_EVENTS}" "${WORKSPACE}/events-service"
+docker build -t "${IMAGE_TAG_REGISTRATION}" "${WORKSPACE}/registration-service"
