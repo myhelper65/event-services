a73cb97cec69b3840ef618246ed24dfdb196b9b2
myhelper65 <myhelper65@gmail.com>
2025-05-27 11:08:56 +0000

    added

--- jenkins/build-dev-docker-images-for-ecr.sh
+++ jenkins/build-dev-docker-images-for-ecr.sh
@@ -1,5 +1,10 @@
 docker build -t "${IMAGE_TAG_EVENTS}" "${WORKSPACE}/events-service"
 docker build -t "${IMAGE_TAG_REGISTRATION}" "${WORKSPACE}/registration-service"
+docker pull mongo:5.0
+docker tag mongo:5.0 mongo:dev
+
+docker pull postgres:14
+docker tag postgres:14 postgres:dev
 
 echo "IMAGE_TAG_EVENTS=${IMAGE_TAG_EVENTS}"
 echo "IMAGE_TAG_REGISTRATION=${IMAGE_TAG_REGISTRATION}"
