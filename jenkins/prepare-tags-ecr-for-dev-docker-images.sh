8e2ffbf1eae2d8b27e60190b71b895f47051e973
myhelper65 <myhelper65@gmail.com>
2025-05-27 12:19:31 +0000

    added

--- jenkins/prepare-tags-ecr-for-dev-docker-images.sh
+++ jenkins/prepare-tags-ecr-for-dev-docker-images.sh
@@ -4,8 +4,13 @@ export IMAGE_TAG_EVENTS="${ECR_REGISTRY}/${APP_REPO_NAME}:events-service-v${MVN_
 MVN_VERSION=$(.${WORKSPACE}/registration-service/target/maven-archiver/pom.properties &&echo $version)
 export IMAGE_TAG_REGISTRATION="${ECR_REGISTRY}/${APP_REPO_NAME}:registration-service-v${MVN_VERSION}-b${BUILD_NUMBER}"
 
-docker tag mongo:5.0 mongo:dev
-docker tag postgres:14 postgres:dev
+export IMAGE_TAG_MONGO="533266982090.dkr.ecr.us-east-1.amazonaws.com/eventservice-repo/eventservice-app-dev:mongo-v${MVN_VERSION}-b${BUILD_NUMBER}"
+export IMAGE_TAG_POSTGRES="533266982090.dkr.ecr.us-east-1.amazonaws.com/eventservice-repo/eventservice-app-dev:postgres-v${MVN_VERSION}-b${BUILD_NUMBER}"
+
+helm upgrade --install eventservice-app-release stable-eventservice/eventservice_chart \
+  --namespace eventservice-dev \
+  --set mongo.image="${IMAGE_TAG_MONGO}" \
+  --set postgres.image="${IMAGE_TAG_POSTGRES}"
 
 
 # Debug output
