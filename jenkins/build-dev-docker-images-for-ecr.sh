06ce05793e743727dc35bafc98ebe913f8394607
myhelper65 <myhelper65@gmail.com>
2025-05-27 15:46:27 +0000

    add

--- jenkins/build-dev-docker-images-for-ecr.sh
+++ jenkins/build-dev-docker-images-for-ecr.sh
@@ -3,8 +3,8 @@ docker build -t "${IMAGE_TAG_REGISTRATION}" "${WORKSPACE}/registration-service"
 # docker build -t "${IMAGE_TAG_MONGO}" "${WORKSPACE}/mongo"
 # docker build -t "${IMAGE_TAG_POSTGRES}" "${WORKSPACE}/postgres"
 
-docker pull mongo:5.0
+# docker pull mongo:5.0
 docker tag mongo:5.0 "${IMAGE_TAG_MONGO}"
 
-docker pull postgres:14
+# docker pull postgres:14
 docker tag postgres:14 "${IMAGE_TAG_POSTGRES}"
--- jenkins/prepare-tags-ecr-for-dev-docker-images.sh
+++ jenkins/prepare-tags-ecr-for-dev-docker-images.sh
@@ -5,19 +5,11 @@ export IMAGE_TAG_EVENTS="${ECR_REGISTRY}/${APP_REPO_NAME}:events-service-v${MVN_
 MVN_VERSION=$(.${WORKSPACE}/registration-service/target/maven-archiver/pom.properties &&echo $version)
 export IMAGE_TAG_REGISTRATION="${ECR_REGISTRY}/${APP_REPO_NAME}:registration-service-v${MVN_VERSION}-b${BUILD_NUMBER}"
 
+# Extract ECR_REGISTRY and APP_REPO_NAME from one of the existing image tags
 ECR_REGISTRY=$(echo "$IMAGE_TAG_REGISTRATION" | cut -d '/' -f1)
-
-# Extract APP_REPO_NAME (e.g., eventservice-repo)
 APP_REPO_NAME=$(echo "$IMAGE_TAG_REGISTRATION" | cut -d '/' -f2 | cut -d ':' -f1)
-
-# Extract BUILD_NUMBER (e.g., 42)
 BUILD_NUMBER=$(echo "$IMAGE_TAG_REGISTRATION" | sed -E 's/.*-b([0-9]+)$/\1/')
 
+# Now use them to export MONGO/POSTGRES tags
 export IMAGE_TAG_MONGO="${ECR_REGISTRY}/${APP_REPO_NAME}:mongo-b${BUILD_NUMBER}"
 export IMAGE_TAG_POSTGRES="${ECR_REGISTRY}/${APP_REPO_NAME}:postgres-b${BUILD_NUMBER}"
-
-# Debug output
-echo "Tag for events-service:        [$IMAGE_TAG_EVENTS]"
-echo "Tag for registration-service:  [$IMAGE_TAG_REGISTRATION]"
-echo "Tag for mongo:                 [$IMAGE_TAG_MONGO]"
-echo "Tag for postgres:              [$IMAGE_TAG_POSTGRES]"
