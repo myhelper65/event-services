3f2d522e919a4663b5f1130c18c2aa3fc03d5c7b
myhelper65 <myhelper65@gmail.com>
2025-05-27 11:47:18 +0000

    ad

--- jenkins/prepare-tags-ecr-for-dev-docker-images.sh
+++ jenkins/prepare-tags-ecr-for-dev-docker-images.sh
@@ -4,8 +4,8 @@ export IMAGE_TAG_EVENTS="${ECR_REGISTRY}/${APP_REPO_NAME}:events-service-v${MVN_
 MVN_VERSION=$(.${WORKSPACE}/registration-service/target/maven-archiver/pom.properties &&echo $version)
 export IMAGE_TAG_REGISTRATION="${ECR_REGISTRY}/${APP_REPO_NAME}:registration-service-v${MVN_VERSION}-b${BUILD_NUMBER}"
 
-IMAGE_TAG_MONGO="${ECR_REGISTRY}/${APP_REPO_NAME}:mongo-v${MVN_VERSION}-b${BUILD_NUMBER}"
-IMAGE_TAG_POSTGRES="${ECR_REGISTRY}/${APP_REPO_NAME}:postgres-v${MVN_VERSION}-b${BUILD_NUMBER}"
+IMAGE_TAG_MONGO=docker tag mongo:5.0 mongo:dev
+IMAGE_TAG_POSTGRES=docker tag postgres:14 postgres:dev
 
 
 # Debug output
