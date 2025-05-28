b2daab300e7d3138aec4969bd310562af1c215cf
myhelper65 <myhelper65@gmail.com>
2025-05-28 13:47:44 +0000

    added sh file

---
+++ jenkins/prepare-tags-ecr-for-qa-docker-images.sh
@@ -0,0 +1,11 @@
+MVN_VERSION=$(.${WORKSPACE}/events-service/target/maven-archiver/pom.properties &&echo $version)
+export IMAGE_TAG_EVENTS="${ECR_REGISTRY}/${APP_REPO_NAME}:events-service-qa-v${MVN_VERSION}-b${BUILD_NUMBER}"
+
+MVN_VERSION=$(.${WORKSPACE}/registration-service/target/maven-archiver/pom.properties &&echo $version)
+export IMAGE_TAG_REGISTRATION="${ECR_REGISTRY}/${APP_REPO_NAME}:registration-service-qa-v${MVN_VERSION}-b${BUILD_NUMBER}"
+
+# export IMAGE_TAG_MONGO="${ECR_REGISTRY}/${APP_REPO_NAME}:mongo-v${MVN_VERSION}-b${BUILD_NUMBER}"
+# export IMAGE_TAG_POSTGRES="${ECR_REGISTRY}/${APP_REPO_NAME}:postgres-v${MVN_VERSION}-b${BUILD_NUMBER}"
+
+
+
