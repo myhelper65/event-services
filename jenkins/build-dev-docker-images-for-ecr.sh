f09f348b698917249134b5be1cb8c7cdec228b00
myhelper65 <myhelper65@gmail.com>
2025-05-28 11:32:08 +0000

    addede

--- jenkins/build-dev-docker-images-for-ecr.sh
+++ jenkins/build-dev-docker-images-for-ecr.sh
@@ -3,11 +3,9 @@ docker build -t "${IMAGE_TAG_REGISTRATION}" "${WORKSPACE}/registration-service"
 # docker build -t "${IMAGE_TAG_MONGO}" "${WORKSPACE}/mongo"
 # docker build -t "${IMAGE_TAG_POSTGRES}" "${WORKSPACE}/postgres"
 
-docker pull mongo:5.0
-docker tag mongo:5.0 mongo:dev
+# docker pull mongo:5.0
+# docker tag mongo:5.0 mongo:dev
 
-docker pull postgres:14
-docker tag postgres:14 postgres:dev
+# docker pull postgres:14
+# docker tag postgres:14 postgres:dev
 
-# echo "IMAGE_TAG_EVENTS=${IMAGE_TAG_EVENTS}"
-# echo "IMAGE_TAG_REGISTRATION=${IMAGE_TAG_REGISTRATION}"
--- jenkins/push-dev-docker-images-to-ecr.sh
+++ jenkins/push-dev-docker-images-to-ecr.sh
@@ -1,5 +1,3 @@
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_REGISTRY} 
 docker push "${IMAGE_TAG_EVENTS}"
 docker push "${IMAGE_TAG_REGISTRATION}"
-# docker push "${IMAGE_TAG_MONGO}"
-# docker push "${IMAGE_TAG_POSTGRES}"
