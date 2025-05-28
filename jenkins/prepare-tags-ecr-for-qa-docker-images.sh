759fe0dc2ba76fe6e5957a088c7c08390872ec36
myhelper65 <myhelper65@gmail.com>
2025-05-28 14:19:31 +0000

    added jenkinsfile petclinic-weekly-qa for release branch

--- jenkins/prepare-tags-ecr-for-qa-docker-images.sh
+++ jenkins/prepare-tags-ecr-for-qa-docker-images.sh
@@ -4,8 +4,7 @@ export IMAGE_TAG_EVENTS="${ECR_REGISTRY}/${APP_REPO_NAME}:events-service-qa-v${M
 MVN_VERSION=$(. ${WORKSPACE}/registration-service/target/maven-archiver/pom.properties &&echo $version)
 export IMAGE_TAG_REGISTRATION="${ECR_REGISTRY}/${APP_REPO_NAME}:registration-service-qa-v${MVN_VERSION}-b${BUILD_NUMBER}"
 
-# export IMAGE_TAG_MONGO="${ECR_REGISTRY}/${APP_REPO_NAME}:mongo-v${MVN_VERSION}-b${BUILD_NUMBER}"
-# export IMAGE_TAG_POSTGRES="${ECR_REGISTRY}/${APP_REPO_NAME}:postgres-v${MVN_VERSION}-b${BUILD_NUMBER}"
+
 
 
 
